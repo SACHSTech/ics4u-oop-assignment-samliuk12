e47dd39a37bab122082b6ec457106e536f0d9d0f
samliu12 <sam.liu21@ycdsbk12.ca>
2020-12-17 18:13:24 +0000

    coded gym owner class

--- src/PokemonSimulation/GymOwner.java
+++ src/PokemonSimulation/GymOwner.java
@@ -7,7 +7,45 @@ public class GymOwner extends Person {
   private GymLocation gymLocation;
 
   public GymOwner(String name, int age, Pokemon activePokemon, GymLocation gymLocation) {
+    super(name, age, activePokemon);
+    victories = 0;
+    defeats = 0;
+    this.gymLocation = gymLocation;
+  }
 
+  public String toString() {
+    String string = super.toString();
+    string += "\nVictories: " + victories + "\nDefeats: " + defeats + "\nGym location: " + gymLocation.getCity();
+    return string;
+  }
+
+  public int getVictories() {
+    return victories;
+  }
+
+  public int getDefeats() {
+    return defeats;
+  }
+
+  public void addVictory() {
+    victories++;
+  }
+
+  public void addDefeat() {
+    defeats++;
+    giveBadge();
+  }
+
+  public GymLocation getGymLocation() {
+    return gymLocation;
+  }
+
+  public void acceptOpponent() {
+    System.out.println("A new opponent has been accepted");
+  }
+
+  private void giveBadge() {
+    System.out.println("A new badges has been given");
   }
 
 }
